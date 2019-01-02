02509993b77b8e722adc2f44d8e339ab4d6df6c8
robertschaap <robert.schaap@mac.com>
2019-01-02 14:35:45 +0100

    deck.go: added import method

--- cards/deck.go
+++ cards/deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -39,4 +40,15 @@ func (d deck) saveToFile(filename string) error {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
