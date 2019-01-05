d04e39739890c5519581107b49a0af7c1bad65ac
robertschaap <robert.schaap@mac.com>
2019-01-05 20:44:33 +0100

    deck_test: added save to and from file tests

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,4 +19,20 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected Four of Clubs but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	f := "_decktesting"
+	os.Remove(f)
+
+	d := newDeck()
+	d.saveToFile(f)
+
+	loadedDeck := newDeckFromFile(f)
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards but got %v", len(loadedDeck))
+	}
+
+	os.Remove(f)
 }
