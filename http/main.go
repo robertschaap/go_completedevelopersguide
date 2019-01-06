078b19ea266aa090d6693ba2ab29a2e7ce775d4f
robertschaap <robert.schaap@mac.com>
2019-01-06 19:06:26 +0100

    http: added logwriter

--- http/main.go
+++ http/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"io"
 	"fmt"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -14,5 +17,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	return len(bs), nil
 }
