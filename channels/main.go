d93a38e61b874878c6b05a468794193809311ed6
robertschaap <robert.schaap@mac.com>
2019-01-09 07:24:35 +0100

    channels: setup channel

--- channels/main.go
+++ channels/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"time"
 	"fmt"
 	"net/http"
 )
@@ -12,18 +13,29 @@ func main()  {
 		"http://stackoverflow",
 	}
 
+	c := make(chan string)
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+
 }
 
-func checkLink(link string)  {
+func checkLink(link string, c chan string)  {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println(link, "might be down")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up")
+	c <- link
 }
